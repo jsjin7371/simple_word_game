c515cd5f44b119795160965381d85e4fff1de804
jsj <120244130+jsjin7371@users.noreply.github.com>
2023-11-14 16:46:55 +0900

    add initUserData function

--- lib/models/uesr_data_model.dart
+++ lib/models/uesr_data_model.dart
@@ -4,10 +4,18 @@ class UserDataModel extends ChangeNotifier {
   int timesofTry = 0;
   int timesofAnswer = 0;
 
+  // 유저 데이터 초기화 함수
+  void initUserData() {
+    timesofTry = 0;
+    timesofAnswer = 0;
+  }
+
+  // 시도 횟수 추가 함수
   void addCountofTry() {
     timesofTry++;
   }
 
+  // 정답 맞춘 횟수 추가 함수
   void addCountofAnwser() {
     timesofAnswer++;
   }
